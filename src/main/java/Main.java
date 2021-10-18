6827262943ffda950e15f9b785e9e7991ae40145
Abhishek Kalyan Adupa <abhishek.adupa@columbia.edu>
2021-10-18 13:07:25 -0400

    move it from the new location to somewhere far, returns false

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hey!");
         Board b = new Board(8);
         System.out.println(b.action(1,1,2,1));
+        System.out.println(b.action(2,1,5,1));
     }
 }
